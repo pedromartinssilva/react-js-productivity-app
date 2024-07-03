352a4d0ca2bbf6ad646d534896b84cd99b494330
pedromartinssilva <contatopedromartinssilva@gmail.com>
2024-07-02 22:23:23 -0300

    feat: todo list elements and style done (missing completed style)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const Todo = () => {
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
     return (
-        <div>
-            Todo
+        <div
+        className="Todo">
+            <p onClick={()=>toggleComplete(task.id)} className={`${task.completed} ? "completed" : ""`}>{task.task}</p>
+            <div className="icons">
+                <FontAwesomeIcon icon={faPenToSquare}
+                onClick={() => editTodo(task.id)}/>
+                <FontAwesomeIcon icon={faTrash}
+                onClick={() => deleteTodo(task.id)} />
+            </div>
         </div>
     )
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoFrom = () => {
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        addTodo(value);
+        setValue("");
+    }
+
     return (
-        <div>
-            TodoFrom
-        </div>
+        <form className="TodoForm" onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            className="todo-input" 
+            value={value}
+            placeholder="Adicione uma tarefa" 
+            onChange={(e) => setValue(e.target.value)}/>
+            <button type="submit" className="todo-btn">Adicionar</button>
+        </form>
     )
 }
